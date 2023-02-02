245391be4dc8035c80816069b900d6f6f15abfba
Axi20 <varagn19@gmail.com>
2023-02-02 19:49:12 +0100

    set to render homeComponent

--- Frontend/src/pages/homepage.jsx
+++ Frontend/src/pages/homepage.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import Effect from '../components/Effect'
-import GotoPage from '../components/GotoPage'
-import SpecialOffers from '../components/SpecialOffers'
 import HomeComponent from '../components/homeComponents/homeComponent';
-import Footer from '../components/Footer';
 
 const HomePage = () => {
   return (
     <section>
-      <Effect />
-      <GotoPage />
-      <SpecialOffers />
-      <Footer/>
+      <HomeComponent/>
     </section>
   )
 }
